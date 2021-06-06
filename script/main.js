ec2720be65b133b8b1339c619258ed5a0d0cbedc
MakeBasic <niko@makebasic.com>
2021-06-06 14:45:03 +0200

    add header main-manu

--- script/main.js
+++ script/main.js
@@ -1,3 +1,16 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 });
+
+$(".topbar-burger-container").click(function() {
+    console.log( "menu" );
+    $(this).toggleClass('cross');
+    $('html').toggleClass('noscroll');
+    $('.main-menu-container').toggleClass('main-menu-opened');
+
+    if ($(this).hasClass("cross")) {
+        $('.topbar-burger-typo').html("Zurück");
+      } else {
+        $('.topbar-burger-typo').html("Menü");
+      }
+});
